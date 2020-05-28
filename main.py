669bd3bedc3a284b70ce12cec38dce69a3a3efba
Ian Bishop <iwbishop@gmail.com>
2020-05-28 17:08:12 -0400

    now updating the files in the main repo directory, then copying to lesson subdirs to back at each stage in the process

---
+++ main.py
@@ -0,0 +1,26 @@
+#DNA Toolkit/Code testing file
+from DNAToolkit import *
+from utilities import colored
+import random
+
+
+randDNAStr = ''.join([random.choice(Nucleotides)
+					for nuc in range(50)])
+#rndDNAStr = "ATTTGCcc"
+
+DNAStr = validateSeq(randDNAStr)
+
+print(f'\nSequence: {DNAStr}\n')
+print(f'[1] + Sequence Length: {len(DNAStr)}\n')
+print(f'[2] + Nucleotide Frequency: {nucleotide_frequency(DNAStr)}\n')
+
+print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+
+print(f"[4] + DNA String + Complement + Reverse Complement:\n5' {DNAStr} 3'")
+print(f"    {''.join(['|' for c in range(len(DNAStr))])}")
+print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
+print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
+
+print(f'[5] + GC content: {gc_content(DNAStr)}%\n')
+print(f'[6] + GC content in windows of size k=5: {gc_content_subsec(DNAStr, k=5)}\n')
+
