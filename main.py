d307045ffe4d428aa7bf7c7b53f33d17f81017c9
Ian Bishop <iwbishop@gmail.com>
2020-05-29 21:38:29 -0400

    finished through lesson 9, including complete bio_seq toolkit

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 #DNA Toolkit/Code testing file
-from DNAToolkit import *
-from utilities import colored
-import random
 
+from bio_seq import bio_seq
+from utilities import read_FASTA, readTextFile, writeTextFile
 
-randDNAStr = ''.join([random.choice(Nucleotides)
-					for nuc in range(50)])
-#rndDNAStr = "ATTTGCcc"
+test_dna = bio_seq()
+test_dna.generate_rnd_seq(40, "RNA")
 
-DNAStr = validateSeq(randDNAStr)
+print(test_dna.get_seq_info())
+print(test_dna.nucleotide_frequency())
+print(test_dna.transcription())
+print(test_dna.seq)
+print(test_dna.reverse_complement())
+print(test_dna.gc_content())
+print(test_dna.gc_content_subsec())
+print(test_dna.translate_seq())
+print(test_dna.codon_usage("L"))
+for rf in test_dna.gen_reading_frames():
+    print(rf)
 
-print(f'\nSequence: {DNAStr}\n')
-print(f'[1] + Sequence Length: {len(DNAStr)}\n')
-print(f'[2] + Nucleotide Frequency: {nucleotide_frequency(DNAStr)}\n')
+print(test_dna.proteins_from_rf(
+    ['A', 'M', 'Q', 'T', 'F', 'A', 'L', 'H', 'Q', 'M', 'C', '_', 'H']))
 
-print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+print(test_dna.all_proteins_from_orfs())
 
-print(f"[4] + DNA String + Complement + Reverse Complement:\n5' {DNAStr} 3'")
-print(f"    {''.join(['|' for c in range(len(DNAStr))])}")
-print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
-print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
+# writeTextFile("test.txt", test_dna.seq)
+# for rf in test_dna.gen_reading_frames():
+#     writeTextFile("test.txt", str(rf), 'a')
 
-print(f'[5] + GC content: {gc_content(DNAStr)}%\n')
-print(f'[6] + GC content in windows of size k=5: {gc_content_subsec(DNAStr, k=5)}\n')
-
-print(f'[7] + Amino acid sequence from DNA: {translate_seq(DNAStr, 0)}\n')
-# print(f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
-
-print('[9] + Reading frames:')
-for frame in gen_reading_frames(DNAStr):
-    print(frame)
-
-print('\n[10] + All proteins in all ORFs:')
-for prot in all_proteins_from_orfs(NM_000207, 0, 0, True):
-    print(f'{prot}')
+# fasta = read_FASTA("fasta_samples.txt")
+# print(fasta)
